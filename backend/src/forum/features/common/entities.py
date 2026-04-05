7c8ea9ed917a955019a92d9810da11ee750b7820
shellcode <example@gmail.com>
2026-04-05 07:54:56 +0300

    style: type hint

--- backend/src/forum/features/common/entities.py
+++ backend/src/forum/features/common/entities.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Self
 
 from forum.features.common.mixins import (
     CreatedAtMixin,
@@ -32,5 +32,5 @@ class CreatedAtEntity(BaseEntity, CreatedAtMixin):
 
 
 class EditableEntity(CreatedAtEntity, EditableMixin):
-    def edited(self, repo: "CRUDRepo[EditableEntity]") -> None:
+    def edited(self, repo: "CRUDRepo[Self]") -> None:
         repo.update(self.id, last_edited_at=datetime.now())
